f28dbad644078adba4032cb1019269a3bd81f204
dd-han <han@dd-han.tw>
2016-05-06 18:34:49 +0800

    save order to sqlite now

--- dbs.py
+++ dbs.py
@@ -34,12 +34,17 @@ class sqliteDB:
     def initOrderDetailTable(this):
         cursor = this.connection.cursor()
         cursor.execute('CREATE TABLE OrdersDetail(\
-            OrderNumber INTEGER PRIMARY KEY NOT NULL, \
-            ID INTEGER NOT NULL, ItemAmout INTEGER)')
+            OrderNumber INTEGER NOT NULL, \
+            ID INTEGER NOT NULL, ItemAmout INTEGER, PRIMARY KEY (OrderNumber,ID))')
 
     def insertOrder(this,orderNumber,ship,info,items):
         cursor = this.connection.cursor()
-        cursor.execute('SELECT OrderNumber FROM `Order` ORDER BY OrderNumber DESC LIMIT 1')
+        this.needCommit = True
+        data = [orderNumber,info[0],info[1],info[2],ship]
+        cursor.execute('INSERT INTO Orders (OrderNumber,Name,Address,Phone,shippment) VALUES (?,?,?,?,?);',data)
+        for i in items:
+            data = [orderNumber,i[0],i[4]]
+            cursor.execute('INSERT INTO OrdersDetail (OrderNumber,ID,ItemAmout) VALUES (?,?,?);',data)
         
     def lookupMaxOrderID(this):
         cursor = this.connection.cursor()
@@ -94,12 +99,11 @@ class orderDB:
 
     def __del__(this):
         if this.needCommit:
-            this.connection.commit()
-        this.connection.close
+            this.sqlCon.forceCommit()
+
 
     def genOrderNum(this):
         curOrder = str(this.sqlCon.lookupMaxOrderID())
-        print(curOrder)
         if (curOrder == "None"):
             return this.strftime(this.t_format,this.now()) + '0'*this.numLength
         else :
@@ -123,6 +127,8 @@ class orderDB:
                 return this.strftime(this.t_format,this.now()) + '0'*this.numLength
 
     def insertOrder(this,orderNumber,ship,info,items):
+        this.sqlCon.insertOrder(orderNumber,ship,info,items)
+        this.sqlCon.forceCommit()
         this.insertToCSV(orderNumber,ship,info,items)
 
     def insertToCSV(this,orderNumber,ship,info,items):
@@ -133,5 +139,4 @@ class orderDB:
             for i in items:
                 csvwriter.writerow([orderNumber,info[0],info[1],info[2],i[0],i[1],i[2],i[4],shippment])
                 shippment = 0
-
 
